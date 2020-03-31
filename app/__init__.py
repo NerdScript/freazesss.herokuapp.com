b846cdae7d6d3597f1582be2072319ea5d6e132f
Gustavo <freazesss@gmail.com>
2020-03-31 12:30:25 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask, Blueprint, render_template
 from app.home import home
-from app.about import about
 
 app = Flask(__name__)
 app.register_blueprint(home)
-app.register_blueprint(about)
 
 @app.errorhandler(404)
 def not_foundpage(e):
